4208065d120699b43d82dc3175d31f52b600af11
Igor Escodro <escodro@outlook.com>
2020-01-09 11:18:07 -0300

    :tada: Initial commit
    Base Android Project created

---
+++ app/src/main/java/com/escodro/dynaaminen/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.escodro.dynaaminen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
