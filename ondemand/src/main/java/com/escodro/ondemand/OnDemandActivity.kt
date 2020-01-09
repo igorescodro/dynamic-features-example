e9809c47a41e945a254a88cf538eca2c077d7bc4
Igor Escodro <escodro@outlook.com>
2020-01-09 15:28:02 -0300

    :lipstick: Create simple UI for On Demand module
    Simple Activity created

---
+++ ondemand/src/main/java/com/escodro/ondemand/OnDemandActivity.kt
@@ -0,0 +1,12 @@
+package com.escodro.ondemand
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnDemandActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_demand)
+    }
+}
